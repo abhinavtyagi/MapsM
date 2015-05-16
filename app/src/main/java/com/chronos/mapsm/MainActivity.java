25ef41cd35b5c4a51703c495949a83bdfeb8674f
abhinav <abhityagi85@gmail.com>
2015-05-17 02:15:07 +0530

    Initial commit

---
+++ app/src/main/java/com/chronos/mapsm/MainActivity.java
@@ -0,0 +1,25 @@
+package com.chronos.mapsm;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
